c2d8ccc3cda1429a281ab19228e357d4727c578a
Yan Sun <yans3@illinois.edu>
2025-04-01 00:30:14 +0200

    remove pcimem

--- sw/pcimem/monitor_fpga.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-sudo setpci -s 27:00.1 COMMAND=0x02
-#sudo watch -n 1 ./pcimem /sys/devices/pci0000\:27/0000\:27\:00.1/resource2 32768 d*100
-sudo watch -n 1 ./pcimem /sys/devices/pci0000\:27/0000\:27\:00.1/resource2 0 d*100
